25074db870738e168b0c9958f3a7ae1133620534
harriperhana <harri.huovinen@gmail.com>
2025-06-24 23:07:39 +0300

    kinda-user-friendly menu structure

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-# Why?
-Somehow I have ended up to own a quite large collection of DVD movies but doesn't really have any convenient way to watch em. So instead of everytime setting up my laptop - which have the only household optical drive - with external screen I just make an image of the movie so I can stream it through my network and watch it with any device. So with this script it's more straightforward to make the image and even do a whole bunch at one time.
-
 # Usage
 The only prerequisite is `dd`, which you most probably already have, but in case it's missing, you must install it first.
 
@@ -17,7 +14,14 @@ The only prerequisite is `dd`, which you most probably already have, but in case
 
 After you have satisfied the requirements, just set the script executable with `chmod +x dvdimager.sh` and start destroying disks!
 
-Script defaults using `/dev/cdrom` symlink, but if you have multiple drives you can pass wanted one with parameter, for example...
+Script defaults using `/dev/cdrom` symlink, but if you have multiple drives, you can pass wanted one with parameter for example...
 ```
 ./dvdimager.sh sr1
 ```
+
+# Why?
+Somehow, I have ended up with quite a large collection of DVD movies, but doesn't really have any convenient way to watch em.
+
+Instead of setting up my laptop everytime - which happen to have the only household optical drive - with external screen, I just make an image of the movie, so I can stream it through my network and watch it with any device.
+
+Without further explanation; that's why this script exists since it makes the image creation more straightforward and eases up the process if there's more than one in queue.
--- dvdimager.sh
+++ dvdimager.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
+header() {
+	title="[ $1 ]"
+	len=${#title}
+	padding=$((($(tput cols) - len) / 2))
+	extra=$(($(tput cols) - (padding * 2 + len)))
+	echo
+	echo
+	printf '·%.0s' $(seq 1 $padding)
+	echo -n "$title"
+	printf '·%.0s' $(seq 1 $((padding + extra)))
+	echo
+	echo
+	echo -ne "\033]0;$1\007"
+}
+
+# kekw xD
+header "dvdimager"
+
 # Check prerequisites
 if ! command -v dd &> /dev/null; then
-    echo "Install dd first!"
-    exit 1
+	echo "Install dd first!"
+	echo
+	exit 1
 fi
 
 # Set dev variable from user parameter or default to cdrom
@@ -11,32 +30,89 @@ dev=${1:-cdrom}
 
 # Check if the chosen device exists
 if [ ! -e /dev/"$dev" ]; then
-    echo "Device /dev/$dev not found!"
-    exit 1
+	echo "Device /dev/$dev not found!"
+	echo
+	exit 1
 fi
 
 # Fetch media metadata
-isoinfo=$(isoinfo -d -i /dev/"$dev" 2>/dev/null)
-
-# Check that previous command succeeded
-if [ $? -ne 0 ]; then
-    echo "Failed to read isoinfo!"
-    exit 1
+if ! isoinfo=$(isoinfo -d -i /dev/"$dev" 2>/dev/null); then
+	echo "Failed to read isoinfo!"
+	echo
+	exit 1
 fi
+
+# Default output filename
+of="$(echo "$isoinfo" | grep -Po '\bVolume id: \K(.+)\b').iso"
+
+# Confirm or change output filename
+read -re -i "$of" -p "Output filename: " prompt
+if [ -n "$prompt" ]; then
+	of="$prompt"
+fi
+
+# Handle file existence crisis
+while [ -e "$of" ]; do
+	echo
+	read -rp "File '$of' exists, would you like to [o]verwrite, [r]ename or [c]ancel? " prompt
+	case "$prompt" in
+		[oO])
+			rm -f "$of"
+			echo "Existing file removed, continuing..."
+			;;
+		[rR])
+			read -re -i "$of" -p "Enter new filename: " of
+			of="$of"
+			;;
+		[cC])
+			echo "All right! Take care now! Bye-bye, then!"
+			echo
+			exit 0
+			;;
+		*)
+			echo "Invalid command. Come on, you can do this!"
+			;;
+	esac
+done
+echo
 
 # Build parameters array
 parameters=("if=/dev/$dev")
-parameters+=("of=$(echo "$isoinfo" | grep -Po '\bVolume id: \K(.+)\b').iso")
+parameters+=("of=$of")
 parameters+=("bs=$(echo "$isoinfo" | grep -Po '\bLogical block size is: \K([0-9]+)\b')")
 parameters+=("count=$(echo "$isoinfo" | grep -Po '\bVolume size is: \K([0-9]+)\b')")
 parameters+=("status=progress")
 
-echo "${parameters[@]}"
+# Read disc contents before writing image
+# (somewhy this is needed sometimes to prevent dd I/O failures)
+echo -n "Reading disc files... "
+if isoinfo -i /dev/"$dev" &>/dev/null; then
+	echo "OK!"
+else
+	echo "failed :("
+	echo
+	exit 1
+fi
 
-# Start actual imaging process
+# Start the actual imaging process
 dd "${parameters[@]}"
 
-# Eject media after image is done
+# Eject media after the image is done
 eject "/dev/$dev"
 
-echo "Jobs done!"
+# Check if more action is wanted
+while true; do
+	echo
+	read -p "Moar to read? [y]es, [n]o? " prompt
+	case "$prompt" in
+		[yY])
+			exec bash "$0" "$@"
+			;;
+		*)
+			echo
+			echo "Jobs done!"
+			echo
+			exit 0
+			;;
+	esac
+done
